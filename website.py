7cad914f1ce65628b9652b81eacad29336b05f51
nopalfi <nopalfahriislami@yahoo.com>
2022-11-10 21:42:56 +0800

    kodebaru/refactoring code

--- website.py
+++ website.py
@@ -3,53 +3,33 @@ import math
 
 app=Flask(__name__)
 
-@app.route("/salat", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def salat():
     if request.method == "GET":
         return render_template('salat.html')
     elif request.method == "POST":
-        deklinasi_jam_1 = int(request.form["kds"])
-        deklinasi_menit_1 = int(request.form["kdsm"])
-        deklinasi_detik_1 = int(request.form["kdsd"])
-        deklinasi_1 = (deklinasi_jam_1) + (deklinasi_menit_1/60) + (deklinasi_detik_1/3600)
-        rdeklinasi_subuh = math.radians(deklinasi_1)
+        deklinasi_jam_1, deklinasi_menit_1, deklinasi_detik_1 = request_form("kds", "kdsm", "kdsd")
+        rdeklinasi_subuh = math.radians((deklinasi_jam_1) + (deklinasi_menit_1/60) + (deklinasi_detik_1/3600))
 
-        deklinasi_jam_2 = int(request.form["kda"])
-        deklinasi_menit_2 = int(request.form["kdam"])
-        deklinasi_detik_2 = int(request.form["kdad"])
-        deklinasi_2 = (deklinasi_jam_2) + (deklinasi_menit_2/60) + (deklinasi_detik_2/3600)
-        rdeklinasi_asar = math.radians(deklinasi_2)
+        deklinasi_jam_2, deklinasi_menit_2, deklinasi_detik_2 = request_form("kda", "kdam", "kdad")
+        rdeklinasi_asar = math.radians((deklinasi_jam_2) + (deklinasi_menit_2/60) + (deklinasi_detik_2/3600))
 
-        deklinasi_jam_3 = int(request.form["kdm"])
-        deklinasi_menit_3 = int(request.form["kdmm"])
-        deklinasi_detik_3 = int(request.form["kdmd"])
-        deklinasi_3 = (deklinasi_jam_3) + (deklinasi_menit_3/60) + (deklinasi_detik_3/3600)
-        rdeklinasi_magrib = math.radians(deklinasi_3)
+        deklinasi_jam_3, deklinasi_menit_3, deklinasi_detik_3 = request_form("kdm", "kdmm", "kdmd")
+        rdeklinasi_magrib = math.radians((deklinasi_jam_3) + (deklinasi_menit_3/60) + (deklinasi_detik_3/3600))
 
-        deklinasi_jam_4 = int(request.form["kdi"])
-        deklinasi_menit_4 = int(request.form["kdim"])
-        deklinasi_detik_4 = int(request.form["kdid"])
-        deklinasi_4 = (deklinasi_jam_4) + (deklinasi_menit_4/60) + (deklinasi_detik_4/3600)
-        rdeklinasi_isya = math.radians(deklinasi_4)
+        deklinasi_jam_4, deklinasi_menit_4, deklinasi_detik_4 = request_form("kdm", "kdmm", "kdmd")
+        rdeklinasi_isya = math.radians((deklinasi_jam_4) + (deklinasi_menit_4/60) + (deklinasi_detik_4/3600))
 
-        lintang_jam = int(request.form["klj"])
-        lintang_menit = int(request.form["klm"])
-        lintang_detik = int(request.form["kld"])
-        lintang = (lintang_jam) + (lintang_menit/60) + (lintang_detik/3600)
-        rlintang = math.radians(lintang)
+        lintang_jam, lintang_menit, lintang_detik = request_form("klj", "klm", "kld")
+        lintang = math.radians((lintang_jam) + (lintang_menit/60) + (lintang_detik/3600))
 
-        bujur_jam = int(request.form["kbj"])
-        bujur_menit = int(request.form["kbm"])
-        bujur_detik = int(request.form["kbd"])
+        bujur_jam, bujur_menit, bujur_detik = request_form("kbj", "kbm", "kbd")
         bujur = (bujur_jam) + (bujur_menit/60) + (bujur_detik/3600)
 
         zona_waktu = int(request.form["kzw"])
-        KWD0 = (zona_waktu) - (bujur)
-        KWD = (KWD0)/15
+        KWD = ((zona_waktu - bujur)/15)
 
-        Hari = int(request.form["khd"])
-        Bulan = int(request.form["kbbm"])
-        Tahun = int(request.form["ktj"])
+        Hari, Bulan, Tahun = request_form("kbj", "kbbm", "ktj")
 
         if Bulan == 1:
             Tahun = Tahun - 1
@@ -59,59 +39,62 @@ def salat():
             Bulan = 2 + 12
         A = math.floor (Tahun/100)
         B = 2 + math.floor (A/4) - A
+
         Dsubuh = (Hari-1) + (((21 * 3600)+(0 * 60)+ 0)/86400)
         Dzuhur = Hari + (((5 * 3600)+(0 * 60)+ 0)/86400)
         Dasar = Hari + (((8 * 3600)+(0 * 60)+ 0)/86400)
         Dmagrib = Hari + (((11 * 3600)+(0 * 60)+ 0)/86400)
         Disya = Hari + (((12 * 3600)+(0 * 60)+ 0)/86400)
 
-        eot = perhitungan_equation (Tahun, Bulan, B, Dzuhur)
+        eot = perhitungan_equation(Tahun, Bulan, B, Dzuhur)
         EoT_zuhur = eot
-        eot = perhitungan_equation (Tahun, Bulan, B, Dsubuh)
+        eot = perhitungan_equation(Tahun, Bulan, B, Dsubuh)
         EoT_subuh = eot
-        eot = perhitungan_equation (Tahun, Bulan, B, Dasar)
+        eot = perhitungan_equation(Tahun, Bulan, B, Dasar)
         EoT_asar = eot
-        eot = perhitungan_equation (Tahun, Bulan, B, Dmagrib)
+        eot = perhitungan_equation(Tahun, Bulan, B, Dmagrib)
         EoT_magrib = eot
-        eot = perhitungan_equation (Tahun, Bulan, B, Disya)
+        eot = perhitungan_equation(Tahun, Bulan, B, Disya)
         EoT_isya = eot
 
-        #tinggi_tempat_jam = int(request.form["kt"])
+        # tinggi_tempat_jam = int(request.form["kt"])
         i = 2/60
-        #h
-        #subuh
+
+        # subuh
         tinggi_subuh= -20
         rtinggi_subuh= math.radians(tinggi_subuh)
-        #tinggi_syuruq= 
-        #tinggi_duha=
-        #asar
-        tinggi_asar0= math.radians(abs((lintang)-(deklinasi_2)))
+
+        # asar
+        
+        lintang1 = lintang_jam + (lintang_menit/60) + (lintang_detik/3600)
+        deklinasi_2 = deklinasi_jam_2 + (deklinasi_menit_2/60) + (deklinasi_detik_2/3600)
+        tinggi_asar0= math.radians(abs(lintang1 - deklinasi_2))
         tinggi_asar= math.degrees(math.atan(1/((math.tan(tinggi_asar0))+1)))
         rtinggi_asar= math.radians(tinggi_asar)
-        #magrib
-        tinggi_magrib= -1
-        rtinggi_magrib= math.radians(tinggi_magrib)
-        #isya
-        tinggi_isya= -18
-        rtinggi_isya= math.radians(tinggi_isya)
 
-        #perhitungan
-        #perhitungan waktu subuh
-        t_subuh = math.degrees(math.acos(-math.tan(rlintang)* math.tan(rdeklinasi_subuh)+ 1/math.cos(rlintang)* 1/math.cos(rdeklinasi_subuh)* math.sin (rtinggi_subuh)))
+        # magrib
+        rtinggi_magrib= math.radians(-1)
+        
+        # isya
+        rtinggi_isya= math.radians(-18)
+
+        # perhitungan
+        # perhitungan waktu subuh
+        t_subuh = perhitungan_degree(lintang, rdeklinasi_subuh, rtinggi_subuh)
         t_selesai_subuh = (t_subuh)/15
         pk = 12-(EoT_subuh)-(t_selesai_subuh)+(KWD)+(i)
-        #perhitungan waktu zuhur
+        # perhitungan waktu zuhur
         pk1 = 12-(EoT_zuhur)+(KWD)+(i)
-        #perhitungan waktu asar
-        t_asar = math.degrees(math.acos(-math.tan(rlintang)* math.tan(rdeklinasi_asar)+ 1/math.cos(rlintang)* 1/math.cos(rdeklinasi_asar)* math.sin (rtinggi_asar)))
+        # perhitungan waktu asar
+        t_asar = perhitungan_degree(lintang, rdeklinasi_asar, rtinggi_asar)
         t_selesai_asar = (t_asar)/15
         pk2 = 12-(EoT_asar)+(t_selesai_asar)+(KWD)+(i)
-        #perhitungan waktu magrib
-        t_magrib = math.degrees(math.acos(-math.tan(rlintang)* math.tan(rdeklinasi_magrib)+ 1/math.cos(rlintang)* 1/math.cos(rdeklinasi_magrib)* math.sin (rtinggi_magrib)))
+        # perhitungan waktu magrib
+        t_magrib = perhitungan_degree(lintang, rdeklinasi_magrib, rtinggi_magrib)
         t_selesai_magrib = (t_magrib)/15
         pk3 = 12-(EoT_magrib)+(t_selesai_magrib)+(KWD)+(i)
-        #perhitungan waktu isya
-        t_isya= math.degrees(math.acos(-math.tan(rlintang)* math.tan(rdeklinasi_isya)+ 1/math.cos(rlintang)* 1/math.cos(rdeklinasi_isya)* math.sin (rtinggi_isya)))
+        # perhitungan waktu isya
+        t_isya=  perhitungan_degree(lintang, rdeklinasi_isya, rtinggi_isya)
         t_selesai_isya = (t_isya)/15
         pk4 = 12-(EoT_isya)+(t_selesai_isya)+(KWD)+(i)
         
@@ -152,6 +135,16 @@ def perhitungan_equation (Tahun, Bulan, B, Dzuhur):
     detik = math.floor((detik0)*60)
     eot = Jam + (Menit/60) + (detik/3600)
     return eot
+
+def perhitungan_degree(rlintang, rdeklinasi, rtinggi):
+    t = math.degrees(math.acos(-math.tan(rlintang)* math.tan(rdeklinasi)+ 1/math.cos(rlintang)* 1/math.cos(rdeklinasi)* math.sin (rtinggi)))
+    return t
+
+def request_form(name1, name2, name3):
+    x = int(request.form[name1])
+    y = int(request.form[name2])
+    z = int(request.form[name3])
+    return x,y,z
 
 if __name__ == '__main__':
     app.run()
